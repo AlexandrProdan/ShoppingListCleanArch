b23a975fba7986b357c9f841e8623829f38fb148
Alexandr <prodan.alexandr.simion@gmail.com>
2024-01-21 16:02:57 +0200

    Adapter for ShopList (ShopListAdapter) added.  Adapter's methods implemented using binding. L25

---
+++ app/src/main/java/com/example/shappinglistcleanarch1/presentation/ShopListAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.shappinglistcleanarch1.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shappinglistcleanarch1.databinding.ItemShopEnabledBinding
+import com.example.shappinglistcleanarch1.domain.ShopItem
+
+class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+
+    val list = listOf<ShopItem>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemShopEnabledBinding.inflate(inflater, parent, false)
+        return ShopItemViewHolder(binding.root)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
+        val shopItem = list[position]
+
+        holder.binding.tvName.text = shopItem.name
+        holder.binding.tvCounter.text = shopItem.count.toString()
+
+        holder.itemView.setOnLongClickListener{
+            true
+        }
+    }
+
+    class ShopItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val binding: ItemShopEnabledBinding = ItemShopEnabledBinding.bind(view)
+
+    }
+
+
+
+}
